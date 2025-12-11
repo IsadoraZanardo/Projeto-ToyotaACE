e08b1e24ac24f8ab7da570d77dd270a771d1a8c3
Isadora Zanardo <isaszanardo@outlook.com>
2025-12-11 20:39:17 -0300

    sync

--- App/Front_toyota_ace/app/dadosPessoais/page.tsx
+++
@@ -1,9 +0,0 @@
-import Estrutura from "../componentes/Estrutura"
-
-export default function Teste(){
-    return(
-        <Estrutura>
-            <p>Aqui é a página de estrutura</p>
-        </Estrutura>
-    )
-}
