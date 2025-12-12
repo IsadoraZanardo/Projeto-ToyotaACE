31e05038adad48fc2d0409600d938fc23ffaac35
Isadora Zanardo <isaszanardo@outlook.com>
2025-12-11 21:34:37 -0300

    11-12-2025

--- Aplicativo/Front_toyota_ace/app/homepage/page.tsx
+++ Aplicativo/Front_toyota_ace/app/homepage/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Header from "../componentes/Header"
 import HeroSection from "../componentes/HeroSection"
 import Backdrop from "../componentes/Backdrop"
@@ -6,22 +8,55 @@ import Login from "../login/page"
 import Rodape from "../componentes/ui/Rodape"
 import MenuBotoes from "../componentes/MeusBotoes"
 
-export default function Home(){
-  return(
-   <div className=''>
-    <Header/> 
-    <Backdrop/>
-    <div className='h-screen flex items-center p-8'>
-      <Card/>
-    </div>
-    {/* Seção principal do conteúdo */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-        <div className="flex flex-col lg:flex-row justify-between">
-          
-         
-        </div>
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+
+  const botaoClasse =
+    "w-full px-6 py-4 bg-red-200 text-white rounded-lg font-semibold hover:bg-red-700 transition text-center"
+
+  return (
+    <div className="">
+      <Header />
+      <Backdrop />
+
+      <div className="h-screen flex items-center p-8">
+        <Card />
       </div>
-    <Rodape/>
-    </div>
-  )
+
+      {/* Seção principal */}
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+
+        {/* GRID 3 colunas ocupando toda largura */}
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 w-full">
+
+          <a
+            onClick={() => router.push("http://localhost:3000/dadosPessoais")}
+            className={botaoClasse}
+          >
+            Financiamento
+          </a>
+
+          <a
+            onClick={() => router.push("http://localhost:3000/financiamento")}
+            className={botaoClasse}
+          >
+            Dados Pessoais
+          </a>
+
+          <a
+            onClick={() => router.push("http://localhost:3000/fichaTecnica")}
+            className={botaoClasse}
+          >
+            Agendar Retirada
+          </a>
+
+        </div>
+
+      </div>
+
+      <Rodape />
+    </div>
+  )
 }
